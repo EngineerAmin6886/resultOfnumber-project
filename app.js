03b35da3a55b7e08884bc9e23863e794c9d50e02
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-07-04 17:59:54 +0330

    result project
    takmil project v barsi code ha.

---
+++ app.js
@@ -0,0 +1,17 @@
+//result of number project
+// 1402/04/13
+//gr:Amin
+
+// js code result [START]
+function result(num1, num2) {
+  // js code outpot [START]
+  let outpot = [
+    num1 + num2,
+    num1 - num2,
+    num1 * num2,
+    num1 / num2,
+    num1 % num2,
+  ];
+  console.log(outpot);
+}
+result(8, 5);
